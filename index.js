547e61132db4d6223798058f1ac79c96e1d15e37
EGRrqq <kakandro1337@gmail.com>
2022-11-08 15:00:26 +0300

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -30,6 +31,8 @@ morgan.token('req-body', req => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(cors())
+app.use(express.static('build'))
 
 app.get('/info', (req, res) => {
     res.send(`
